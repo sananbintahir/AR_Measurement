6a50e3d6ab03c3d8f659b9f8784edfed461e2393
Sanan bin Tahir <sananbintahir@gmail.com>
2020-07-13 07:37:21 +0200

    Initial commit
    Initial

---
+++ Assets/Scripts/DistanceMeasure.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class DistanceMeasure : MonoBehaviour
+{
+
+    [SerializeField] private Text distanceText;
+    public GameObject spawnObject;
+    private PlacementIndicator placementIndicator;
+    private Vector3 initialPosition, finalPosition;
+    private bool objectPlaced;
+    private float distance;
+    private LineRenderer lineRend, line;
+    private int numLines;
+    public Material mat;
+    private Text canvasText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectPlaced = false;
+        numLines = 0;
+        lineRend = GetComponent<LineRenderer>();
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began && !objectPlaced)
+        {
+            lineRend.positionCount = 2;
+            GameObject obj = Instantiate(spawnObject, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            initialPosition = obj.transform.position;
+            objectPlaced = true;
+        }
+        else if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began && objectPlaced)
+        {
+            numLines++;
+            lineRend.positionCount = 0;
+            GameObject obj = Instantiate(spawnObject, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            finalPosition = obj.transform.position;
+            objectPlaced = false;
+            CreatLine();
+            obj.transform.Find("Canvas").gameObject.SetActive(true);
+            obj.transform.Find("Canvas/Text").GetComponent<Text>().text = Vector3.Distance(finalPosition, initialPosition).ToString("F2") + " m";
+        }
+        if (objectPlaced)
+        {
+            distance = Mathf.Round(Vector3.Distance(placementIndicator.transform.position, initialPosition) * 100.0f) * 0.01f;
+            distanceText.text = distance.ToString();
+            lineRend.SetPosition(0, initialPosition);
+            lineRend.SetPosition(1, placementIndicator.transform.position);
+        }
+
+        void CreatLine()
+        {
+            line = new GameObject("Line" + numLines).AddComponent<LineRenderer>();
+            line.material = mat;
+            line.startWidth = 0.01f;
+            line.useWorldSpace = false;
+            line.endWidth = 0.01f;
+            line.SetPosition(0, initialPosition);
+            line.SetPosition(1, finalPosition);
+        }
+    }
+}
---
+++ Assets/Scripts/PlacementIndicator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+    private ARRaycastManager managerRay;
+    private GameObject indicatorObject;
+
+    private void Start()
+    {
+        managerRay = FindObjectOfType<ARRaycastManager>();
+        indicatorObject = transform.GetChild(0).gameObject;
+
+        indicatorObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        managerRay.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        if (hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if (!indicatorObject.activeInHierarchy)
+            {
+                indicatorObject.SetActive(true);
+            }
+        }
+    }
+}
